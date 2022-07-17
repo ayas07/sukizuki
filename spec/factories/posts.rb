bc44b1331cab705c9e399f65c5157d612ae49df0
ayas07 <ayakasugiyama07@gmail.com>
2022-07-17 10:02:53 +0900

    Postモデルの単体テスト実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :post do
-    
+    text       { Faker::Lorem.sentence }
+    feeling_id { Faker::Number.between(from: 1, to: 11) }
+    color_id   { Faker::Number.between(from: 1, to: 9) }
+    association :user
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '新規投稿' do
+    context '新規投稿できるとき' do
+      it 'すべての値が正しく入力されていれば投稿できる' do
+        expect(@post).to be_valid
+      end
+      it '画像が選択されていなくても投稿できる' do
+        @post.image = nil
+        expect(@post).to be_valid
+      end
+    end
+    context '新規投稿できないとき' do
+      it 'テキストが空では投稿できない' do
+        @post.text = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include('テキストを入力してください') 
+      end
+      it '気持ちに「---」が選択されていると投稿できない' do
+        @post.feeling_id = 0
+        @post.valid?
+        expect(@post.errors.full_messages).to include('気持ちを選択してください') 
+      end
+      it '色に「---」が選択されていると投稿できない' do
+        @post.color_id = 0
+        @post.valid?
+        expect(@post.errors.full_messages).to include('色を選択してください') 
+      end     
+      it 'ユーザーが紐付いていないと投稿できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Userを入力してください') 
+      end
+    end
+  end
 end
